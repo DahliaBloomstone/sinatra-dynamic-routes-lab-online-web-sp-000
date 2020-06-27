4a6a879836c011f32d6cae6551c07b6f8c5f6e9f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 01:49:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
 
 
 
-  get "/multiply/:num1/:num2" do
+  get "/operation/:num1/:num2" do
     @num1 = params[:num1].to_f
     @num2 = params[:num2].to_f
     return "#{@num1 * @num2}"
