13f76deec95d72824191192b414baf775edf87a5
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 01:43:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "#{@user_name}.reverse"
+  end
 
+  # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_f
+    @num2 = params[:num2].to_f
+    return "#{@num1 * @num2}"
+  end
 end
