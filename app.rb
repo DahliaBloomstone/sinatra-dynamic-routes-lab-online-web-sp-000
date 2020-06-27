e252313723eed5905d4ac781708355aed64b6c2e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 01:46:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
-    @name = params[:name]
-    @name.reverse
+    @number = params[:number]
+    @phrase = params[:phrase]
     end
 
 
