bef0dc37336ae34fce1e700ee77027d822ac4ea0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 01:51:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,8 @@ class App < Sinatra::Base
         @number.times do
           string += @phrase
     end
+    string
+  end 
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
